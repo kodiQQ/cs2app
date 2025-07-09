b7f8f8fc34f26b88fc146063525d94830a2b35b4
KamilMieczkowski <kamil.mieczkowski7@gmail.com>
2025-07-09 18:50:28 +0200

    Add post_save signal to auto-create Wallet for new users

--- caseClicker-backend/cases/apps.py
+++ caseClicker-backend/cases/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CasesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cases'
+class CasesConfig(AppConfig):
+    name = 'cases'
+    def ready(self):
+        import cases.signals
--- caseClicker-backend/config/settings.py
+++ caseClicker-backend/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core',
     'cases',
+    'cases.apps.CasesConfig'
 ]
 
 MIDDLEWARE = [
